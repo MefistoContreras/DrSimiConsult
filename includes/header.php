900d658040c9c947454c606faeaf5bf918e0d012
MefistoC <carlosteiker@gmail.com>
2020-11-26 08:01:51 -0600

    Created CRUD. I just forgoted do commits every new feature :B

---
+++ includes/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Control citas Dr Simi</title>
+        <!-- Boostrap 4-->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+        <!-- Font Awesome 5 -->
+        <script src="https://kit.fontawesome.com/497566ac46.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <nav class="navbar navbar-dark bg-dark">
+        <div class="container">
+            <a href="index.php" class="navbar-brand">Manejo citas</a>
+        </div>
+
+    </nav>
+</body>
---
+++ index.php
@@ -0,0 +1,72 @@
+<?php include("db.php") ?>
+<?php include("includes/header.php") ?>
+
+<div class="container p-4">
+    <div class="row">
+        <div class="col-md-4">
+
+            <?php if(isset($_SESSION['message'])){?>
+            <div class="alert alert-<?= $_SESSION['message_type']?>
+             alert-dismissible fade show" role="alert">
+                <?= $_SESSION['message'] ?>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <?php session_unset();}?>
+
+            <div class="card card-body">
+                <form action="save_task.php" method="POST">
+                    <div class="form-group">
+                        <input type="text" name="title" class="form-control" placeholder="Paciente" autofocus>
+                    </div>
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            <textarea name="description" class="form-control" rows="2"
+                                placeholder="Descripción del paciente"></textarea>
+                        </div>
+                    </div>
+                    <input type="submit" class="btn btn-success btn-block" name="save_task" value="Save task">
+            </div>
+        </div>
+        </form>
+
+    <div class="col-lg-8">
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Paciente</th>
+                            <th>Descripción</th>
+                            <th>Creada el</th>
+                            <th>Acciones</th>
+                        </tr>
+                        <tbody>
+                            <?php
+                                $query ="SELECT * FROM task";
+                                $result_task = mysqli_query($conn,$query);
+                                
+                                while($row=mysqli_fetch_array($result_task)){?>
+                                    <tr>
+                                        <td><?php echo $row['title'];?></td>
+                                        <td><?php echo $row['description'];?></td>
+                                        <td><?php echo $row['created_at'];?></td>
+                                        <td>
+                                            <a class="btn btn-secondary" href="edit.php?id=<?php echo $row['id'] ?>">
+                                                <i class="fas fa-marker"></i>
+                                            </a>
+                                            <a class="btn btn-danger" href="delete_task.php?id=<?php echo $row['id'] ?>">
+                                                <i class="fa fa-tash-alt"></i>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                
+                                <?php } ?>
+                        </tbody>
+                    </thead>
+                </table>
+    </div>
+    </div>
+
+</div>
+
+<?php include('includes/footer.php') ?>
