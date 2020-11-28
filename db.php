88e1d05d9bcb3439579eebecb27ef38e84bc6a03
MefistoContreras <carlosteiker@gmail.com>
2020-11-27 18:25:47 -0600

    'idk'

--- db.php
+++ db.php
@@ -3,9 +3,9 @@
 session_start();
 
 $conn = mysqli_connect(
-    'us-cdbr-east-02.cleardb.com',
-    'b57f6b0b011e3b',
-    '4af26500',
-    'heroku_067bf9f4b59e761'
+    'localhost',
+    'root',
+    '',
+    'simiconsultas'
 );
 
