774a80aaae0d7e018aebd3cee0c9be25b7661061
MefistoC <carlosteiker@gmail.com>
2020-11-27 21:35:52 -0600

    I hope this db connection works 2

--- db.php
+++ db.php
@@ -3,7 +3,7 @@
 session_start();
 
 $conn = mysqli_connect(
-    'us-cdbr-east-02.cleardb.com:3306',
+    'us-cdbr-east-02.cleardb.com',
     'b8866494029a09',
     '5e38f796',
     'heroku_f87fe3a8ed22866'
